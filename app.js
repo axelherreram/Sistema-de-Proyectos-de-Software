3791d258d7aaadf0ae594029f1c4347773a47902
axelherreram <aherreram22@miumg.edu.gt>
2024-10-28 17:56:57 -0600

    Instalación de  dotenv y configuracion de estas

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// app.js
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./docs/swagger');
-const projectsRouter  = require('./routes/projects');
+const projectsRouter = require('./routes/projects');
+const modulesRouter = require('./routes/modules');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use('/projects', projectsRouter);
+app.use('/modules', modulesRouter); 
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en http://localhost:${port}`);
--- models/index.js
+++ models/index.js
@@ -1,17 +1,15 @@
 const Sequelize = require('sequelize');
-const config = require('../config/config.json')[process.env.NODE_ENV || 'development'];
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const config = require('../config/config')[process.env.NODE_ENV || 'development']; // Cargar configuración según el entorno
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+  port: config.port,
+});
 
 const db = {
   sequelize,
   Sequelize,
-  Project: require('./Project')(sequelize),
-  Module: require('./Module')(sequelize),
-  Phase: require('./Phase')(sequelize),
 };
-
-db.Project.associate(db);
-db.Module.associate(db);
-db.Phase.associate(db);
 
 module.exports = db;

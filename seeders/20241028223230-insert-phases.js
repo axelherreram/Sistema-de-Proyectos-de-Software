0a70bf5b9943b9733b6459bd2552431eef72282e
axelherreram <aherreram22@miumg.edu.gt>
2024-10-28 16:53:22 -0600

    Seeder para la tabla de fases

---
+++ seeders/20241028223230-insert-phases.js
@@ -0,0 +1,17 @@
+// seeders/YYYYMMDDHHMMSS-insert-phases.js
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('phases', [
+      { name: 'Análisis', color: 'lightblue', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Diseño', color: 'green', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Desarrollo', color: 'orange', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Pruebas', color: 'red', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Implementación', color: 'darkblue', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Mantenimiento', color: 'grey', createdAt: new Date(), updatedAt: new Date() },
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('phases', null, {});
+  }
+};

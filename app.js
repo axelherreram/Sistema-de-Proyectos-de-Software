dacba8143170f3e2dd26dc5941b4cb97f148f276
axelherreram <aherreram22@miumg.edu.gt>
2024-10-29 15:33:34 -0600

    prb

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "*"	, 
 }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());

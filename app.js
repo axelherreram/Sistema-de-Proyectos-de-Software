d05f8c43c8e971aecab21f8710d228abb1d69b43
axelherreram <aherreram22@miumg.edu.gt>
2024-10-29 15:54:47 -0600

    prueba

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));

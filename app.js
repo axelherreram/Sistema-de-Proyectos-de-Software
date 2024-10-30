74d426e501e8f5cac232f8f41ee315e24a408aa8
axelherreram <aherreram22@miumg.edu.gt>
2024-10-29 21:04:45 -0600

    Cors para la aplicacion front

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
   origin: "http://localhost:5173",
+  origin: "https://examen-final-wep.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
